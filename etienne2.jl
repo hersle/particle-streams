5b265b579989ca098a383a51b4e879fc1b0ce27b
Herman Sletmoen <hermansletmoen@gmail.com>
2021-04-14 21:17:25 +0200

    More cleanup, removed unused static plots

--- etienne2.jl
+++ etienne2.jl
@@ -4,9 +4,13 @@ using Plots
 using Printf
 gr(dpi=100, fmt=:png);
 
+STATE_PLOT_SIZE = 400
+
 struct Simulation
 	N::Int64
 
+	width::Float64
+	height::Float64
 	radius::Float64
 
 	times::AbstractArray
@@ -17,84 +21,48 @@ struct Simulation
 	scatters::Array{Array{Tuple{Int64, Float64}}}
 end
 
-STATE_PLOT_SIZE = 400
-WIDTH = 15.0
-HEIGHT = 15.0
-V0 = 5.0
-
-function spawn_velocity(pos)
-    #vx = rand(range(5, 6, length=50))
-    vx = V0
-    vy = rand(range(-0, +0, length=50))
-    if pos[1] > 0
-        vx *= -1
-    end
-    return (vx, vy)
+function spawn_velocity(pos, v0)
+	if pos[1] < 0
+		return (+v0, 0) # spawn on left, move to right
+	else
+		return (-v0, 0) # spawn on right, move to left
+	end
 end
 
-function position_is_available(pos, positions; sepdist=2*a)
+function position_is_available(pos, positions, sepdist)
     return minimum(norm(p.-pos) for p in positions) > sepdist
 end
 
-function spawn_particles(N; sepdist=0)
+function spawn_particles(N, width, height, sepdist)
     positions = Array{Tuple{Float64, Float64}}(undef, N)
     velocities = Array{Tuple{Float64, Float64}}(undef, N)
     for n in 1:N
-        pos = (-100, -100-n) # surely outside, will respawn on death test
-        positions[n] = pos
-        velocities[n] = spawn_velocity(positions[n])
+        positions[n] = (-100*width, -(100+4*n*sepdist)) # surely outside, will respawn on death test
+        velocities[n] = (0, 0) # irrelevant, will respawn immediately anyway
     end
     return positions, velocities
 end
 
-function out_of_bounds(pos)
+function out_of_bounds(width, height, pos)
     x, y = pos[1], pos[2]
-    return x < -WIDTH/2 || x > +WIDTH/2 || y > +HEIGHT || y < -50
+    return x < -width/2 || x > +width/2 || y > +height || y < -50
 end
 
-function interact_hard(positions, velocities, dt, acc1s)
-    N = length(positions)
-    for i in 1:N
-        pos1, vel1 = positions[i], velocities[i]
-        for j in i+1:N
-            pos2, vel2 = positions[j], velocities[j]
-            r = norm(pos2 .- pos1)
-            if r < 2*a && dot(vel2.-vel1,pos2.-pos1) <= 0
-                dvel1 = dot(vel1.-vel2,pos1.-pos2) / (r*r) .* (pos1 .- pos2)
-                vel1, vel2 = vel1 .- dvel1, vel2 .+ dvel1
-                velocities[i] = vel1
-                velocities[j] = vel2
-            end
-        end
-    end
-    for i in 1:N
-        if positions[i][2] < 0 && velocities[i][2] < 0
-            velocities[i] = velocities[i] .* (+1, -1)
-        end
-    end
-    for i in 1:N
-        positions[i] = positions[i] .+ velocities[i] .* dt
-    end
-end
-
-function simulate(N, t, interaction, radius; dt=nothing, nmoving=N)
-    mindt = 0.7 * radius / V0 # 0.7 safety factor
-    dt = dt == nothing ? mindt : dt
+function simulate(N, t, radius, width, height, v0)
+    dt = 0.1 * radius / v0 # 0.7 safety factor # 1.0 would mean particle centers could overlap in one step
     
-    println("V0 = ", V0)
-    println("recommended min dt = ", mindt)
+    println("v0 = ", v0)
     println("dt = ", dt)
     
     times = 0:dt:t
     NT = length(times)
     
-    positions, velocities = spawn_particles(N; sepdist=radius)
+    positions, velocities = spawn_particles(N, width, height, radius)
     
     positions_samples = Array{Tuple{Float64, Float64}, 2}(undef, N, NT)
     velocities_samples = Array{Tuple{Float64, Float64}, 2}(undef, N, NT)
 
-	scatterlines = [3*HEIGHT/3, 2*HEIGHT/3, 1*HEIGHT/3]
-	# scatters = []
+	scatterlines = [3*height/3, 2*height/3, 1*height/3]
 	scatters = [[] for i in 1:length(scatterlines)]
     
     function sample(iter, positions, velocities)
@@ -103,8 +71,6 @@ function simulate(N, t, interaction, radius; dt=nothing, nmoving=N)
     end
     
     sample(1, positions, velocities)
-    
-    acc1s = Array{Tuple{Float64, Float64}}(undef, N)
     
     side = 1
     
@@ -146,22 +112,22 @@ function simulate(N, t, interaction, radius; dt=nothing, nmoving=N)
 		end
 
         for n in 1:N
-            if out_of_bounds(positions[n])
+            if out_of_bounds(width, height, positions[n])
                 # respawn
                 if mod(n, 2) == 0
-                    x = -WIDTH/2
-                    y = (HEIGHT/5 - 0) * n/N
+                    x = -width/2
+                    y = (height/5 - 0) * n/N
                 else
-                    x = +WIDTH/2
-                    y = (HEIGHT/5 - 0) * (n+1)/N
+                    x = +width/2
+                    y = (height/5 - 0) * (n+1)/N
                 end
                 pos = (x, y)
                 
-                if position_is_available(pos, positions, sepdist=2.5*radius)
+                if position_is_available(pos, positions, 2.5*radius)
                     x, y = positions[n][1], positions[n][2]
                     
                     positions[n] = pos
-                    velocities[n] = spawn_velocity(pos)
+                    velocities[n] = spawn_velocity(pos, v0)
                     side = mod1(side + 1, 2) # spawn on other side next time
                 else
                     # then try respawning again at next time step (don't force it!)
@@ -170,53 +136,36 @@ function simulate(N, t, interaction, radius; dt=nothing, nmoving=N)
         end
         sample(iter, positions, velocities)
     end
-    println()
+    println() # end progress writer
     
-    return Simulation(N, radius, times, positions_samples, velocities_samples, scatterlines, scatters)
+    return Simulation(N, width, height, radius, times, positions_samples, velocities_samples, scatterlines, scatters)
 end
 
-function plot_geometry(p, scatterlines=nothing)
-	if scatterlines != nothing
-		hline!(p, scatterlines, color=:black, lw=2, alpha=0.25)
-	end
-end
-
-function plot_trajectories(times, trajectories)
-    p = plot_state(times[1], trajectories[:, 1])
-    p = plot(p, title="Trajectories")
-
-    N, T = size(trajectories)
-    for n in 1:N
-        trajectory = trajectories[n, :]
-        plot!(p, trajectory, arrow=:arrow, color=n)
-    end
-    return p
-end
-
-# function plot_state(time, positions; velocities=nothing, scatterlines=nothing)
-function plot_state(sim::Simulation, i; velocities=nothing, scatterlines=nothing)
-	time, positions = sim.times[i], sim.positions[:,i]
+function plot_state(sim::Simulation, i; velocity_scale=0.0, scatterlines=nothing)
+	time, positions, velocities = sim.times[i], sim.positions[:,i], sim.velocities[:,i]
 
     N = size(positions)[1]
     title = @sprintf("State for N = %d at t = %.3f", N, time)
-    p = plot(title=title, xlim=(-WIDTH/2, +WIDTH/2), ylim=(0, HEIGHT), size=(STATE_PLOT_SIZE, STATE_PLOT_SIZE), legend=nothing, xlabel="x", ylabel="y")
-    plot_geometry(p, scatterlines)
-    scatter!(p, positions, color=1:N, markersize=STATE_PLOT_SIZE * sim.radius/(WIDTH))
-    if velocities != nothing
+    p = plot(title=title, xlim=(-sim.width/2, +sim.width/2), ylim=(0, sim.height), size=(STATE_PLOT_SIZE, STATE_PLOT_SIZE), legend=nothing, xlabel="x", ylabel="y")
+	if scatterlines != nothing
+		hline!(p, scatterlines, color=:black, lw=2, alpha=0.25)
+	end
+    scatter!(p, positions, color=1:N, markersize=STATE_PLOT_SIZE * sim.radius/(sim.width))
+    if velocity_scale != 0.0
         for n in 1:N
             pos, vel = positions[n], velocities[n]
-            plot!(p, [pos, pos .+ vel], arrow=:arrow, color=n)
+            plot!(p, [pos, pos .+ vel .* velocity_scale], arrow=:arrow, color=n)
         end
     end
     return p
 end
 
-function plot_scatters(scatters_so_far)
+function plot_scatters(sim::Simulation, scatters_so_far)
 	N = length(scatters_so_far)
 	p = plot(layout=(N, 1))
 	for i in 1:N
 		if length(scatters_so_far[i]) > 0
-			p = histogram!(p, scatters_so_far[i], xlim=(-WIDTH/2, +WIDTH/2), bins=range(-WIDTH/2, stop=+WIDTH/2, length=15), normalized=true, title="Scattering distribution", legend=nothing, subplot=i)
+			p = histogram!(p, scatters_so_far[i], xlim=(-sim.width/2, +sim.width/2), bins=range(-sim.width/2, stop=+sim.width/2, length=15), normalized=true, title="Scattering distribution", legend=nothing, subplot=i)
 		else
 			p = plot!(p)
 		end
@@ -224,7 +173,6 @@ function plot_scatters(scatters_so_far)
 	return p
 end
 
-# function animate_trajectories(times, trajectories, scatterlines, scatters, dt; t=nothing, fps=30)
 function animate_trajectories(sim::Simulation; dt=nothing, t=nothing, fps=30)
 	dt = dt == nothing ? sim.times[2]-sim.times[1] : dt
     t = t == nothing ? sim.times[end] : t
@@ -243,8 +191,7 @@ function animate_trajectories(sim::Simulation; dt=nothing, t=nothing, fps=30)
             print("\rAnimating $N particle(s) in $(length(1:skip:T)) time steps: $(Int(round(t/T*100, digits=0))) %")
         end
 
-        # p2 = plot_state(sim.times[t], trajectories[:, t], scatterlines=sim.scatterlines)
-        p2 = plot_state(sim, t, scatterlines=sim.scatterlines)
+        p2 = plot_state(sim, t, velocity_scale=0.25, scatterlines=sim.scatterlines)
 
 		for j in 1:length(sim.scatters)
 			while scatteri[j] < length(sim.scatters[j]) && sim.scatters[j][scatteri[j]][1] <= t
@@ -253,49 +200,12 @@ function animate_trajectories(sim::Simulation; dt=nothing, t=nothing, fps=30)
 			end
 		end
 
-		p1 = plot_scatters(scatters_so_far)
+		p1 = plot_scatters(sim, scatters_so_far)
         plot(p1, p2, layout=(2, 1), size=(STATE_PLOT_SIZE, STATE_PLOT_SIZE*2))
     end
     println()
     return mp4(anim, "anim.mp4", fps=fps)
 end
 
-function total_energy(positions, velocities)
-    N, T = size(positions)
-    energies = Array{Float64}(undef, T)
-    for t in 1:T
-        energies[t] = 0
-        for n in 1:N
-            v = norm(velocities[n, t])
-            kin = 1/2*m*v^2
-            pos = positions[n, t]
-
-            # Lennard-Jones
-            pot = potential_wall(pos) + 1/2 * potential_parts(n, positions[:, t])
-
-            energies[t] += kin + pot
-        end
-    end
-    return energies
-end
-
-function plot_energy(times, positions, velocities)
-    energies = total_energy(positions, velocities)
-    p = plot(title="Energy", times, energies, ylim=(0, 2*maximum(energies)), label="", xlabel="t", ylabel="E")
-    return p
-end
-
-function plot_triple(times, positions, velocities)
-    p1 = plot_state(times[1], positions[:, 1], velocities=velocities[:, 1])
-    p2 = plot_trajectories(times, positions)
-    p3 = plot_energy(times, positions, velocities)
-    p = plot(p1, p2, p3, size=(1100, 350), layout=(1, 3), bottom_margin=8*Plots.mm)
-    return p
-end
-
-# times, positions, velocities, scatterlines, scatters = simulate(250, 30, interact_hard, dt=0.01)#; dt=0.005)
-# plot_triple(times, positions, velocities)
-# animate_trajectories(times, positions, scatterlines, scatters, 0.1, fps=20)
-
-sim = simulate(50, 10, interact_hard, 0.2, dt=0.01)
+sim = simulate(50, 10, 0.2, 15.0, 15.0, 5.0)
 animate_trajectories(sim, dt=0.1, fps=20)
